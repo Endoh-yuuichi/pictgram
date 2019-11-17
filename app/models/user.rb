9e8586e9fd4e44fe5e624849c14c9af52c203cdf
Endoh yuuichi <test@mail.com>
2019-11-17 07:10:57 +0000

    RAILS04課題提出

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true,format: { with: /[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]/ }
   validates :name, length: { maximum: 15 }
-  #validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i }
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i }
   
   has_secure_password
 end
