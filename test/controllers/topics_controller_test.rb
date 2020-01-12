1df12b169e3ae34575c476b8500bd2b25cb734bc
Endoh yuuichi <test@mail.com>
2020-01-12 04:48:04 +0000

    Rails05課題提出（訂正版）

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class TopicsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get topics_new_url
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
