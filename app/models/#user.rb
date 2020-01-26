6989b1cd9e5df054b9d82afcb5bdaa585cab60cd
Endoh yuuichi <test@mail.com>
2020-01-26 05:09:52 +0000

    RAILS07課題

---
+++ app/models/#user.rb
@@ -0,0 +1,11 @@
+#class User < ApplicationRecord
+#  validates :name, presence: true
+#  validates :email, presence: true,format: { with: /[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]/ }
+#  validates :name, length: { maximum: 15 }
+#  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i }
+#  
+#  has_secure_password
+
+#  has_many :topics
+#  has_many :favorite_topics, through: :favorites, source: 'topic'
+#end
